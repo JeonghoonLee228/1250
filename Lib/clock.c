2119058a16e2c3f385fdca7f2a245921e8f21139
JeonghoonLee228 <henry040819@gmail.com>
2024-03-08 14:31:31 -0700

    ICA07 Created

--- Lib/clock.c
+++ Lib/clock.c
@@ -5,6 +5,15 @@
 #define DEF_BUS_CLOCK 8000000   //default clock speed
 
 unsigned long speed = DEF_BUS_CLOCK;
+
+
+void Clock_EnableOutput(ClockOutDiv);
+{
+if(speed == ClockOutDiv1) Clock_Set8MHZ();
+else if( speed == ClockOutDiv2) Clock_Set20MHZ();
+else if(speed == ClockOutDiv3) Clock-Clock_Set24MHZ();
+else if(speed == ClockOutDiv4) Clock_Set40MHZ();
+}
 void Clock_Set8MHZ(void)
 {
 PLLCTL = 0;
@@ -23,9 +32,25 @@ while(!(CRGFLG & CRGFLG_LOCK_MASK));
 }
 void Clock_Set24MHZ(void)
 {
-
+CLKSEL &= ~(CLKSEL_PLLSEL_MASK);
+      SYNR = 1;
+    REFDV = 2;
+  PLLCTL = PLLCTL_PLLON_MASK | PLLCTL_AUTO_MASK;
+while(!(CRGFLG & CRGFLG_LOCK_MASK));
+ CLKSEL |= CLKSEL_PLLSEL_MASK;
+ speed = 24000000;
 }
 void Clock_Set40MHZ(void)
 {
-    
+    CLKSEL &= ~(CLKSEL_PLLSEL_MASK);
+      SYNR = 4;
+    REFDV = 1;
+  PLLCTL = PLLCTL_PLLON_MASK | PLLCTL_AUTO_MASK;
+while(!(CRGFLG & CRGFLG_LOCK_MASK));
+ CLKSEL |= CLKSEL_PLLSEL_MASK;
+ speed = 40000000;
+}
+unsigned long Clock_GetBusSpeed(void);
+{
+  return speed;
 }
