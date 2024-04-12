2a85d285f1a94ca9ccfaf74341c91df78b23f0fb
JeonghoonLee228 <149446870+JeonghoonLee228@users.noreply.github.com>
2024-04-13 05:01:20 +0900

    Update segs.c

--- Lib/segs.c
+++ Lib/segs.c
@@ -1,9 +1,9 @@
 #include <hidef.h>
 #include "derivative.h"
 #include "segs.h"
-#define Segs_WLATCH   
-    PORTA &= (~0x01); 
-    PORTA |= 0x01;
+#define Segs_WLATCH PORTA &= (~0x01);  PORTA |= 0x01;
+   
+    
 #define Segs_ML PORTA &= (~0x02);
 #define Segs_MH PORTA |= 0x02;
 
@@ -72,13 +72,22 @@ Segs_Normal(addr, value >> 4, Segs_DP_OFF);
 }
 void Segs_16H (unsigned int value, Segs_LineOption row)
 {
+    int numbers [] = {0b1111000000000000, 0b0000111100000000, 0b0000000011110000, 0b0000000000001111};
     int addr = 4*row;
     int counter =0;
+    Segs_Normal(addr, value >>12, Segs_DP_OFF);
+    Segs_Normal(addr+1, (value >> 8), Segs_DP_OFF);
+    Segs_Normal(addr+2, (value >> 4), Segs_DP_OFF);
+    Segs_Normal(addr+3, value, Segs_DP_OFF);
+
+/*
     for(counter = 0; counter <4; counter++)
     {
-    Segs_Normal(addr + counter, (value >> (4 * (3 - counter))) % (16 * (4 - counter)), Segs_DP_OFF);
+        
+    Segs_Normal(addr + counter, (value & numbers[counter]) % (16 * (4 - counter)), Segs_DP_OFF);
+//Segs_Normal(addr + counter, (value >> (4 * (3 - counter))) % (16 * (4 - counter)), Segs_DP_OFF);
 
     }
-    
+    */
 
 }
